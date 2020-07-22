60efd1c034262a0d29ead5f8220594e5d3b7a96c
Rashmi Modhwadia <rashmi.modhwadia@gmail.com>
2020-07-23 09:23:13 +1200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# otel-node
+
+Sample application from Opentelemetryjs.
+[@opentelemetry/exporter-collector](https://github.com/open-telemetry/opentelemetry-js/tree/master/packages/opentelemetry-exporter-collector) 
+[Exporter Collector](https://github.com/open-telemetry/opentelemetry-js/tree/master/packages/opentelemetry-exporter-collector) 
+
+## Installation
+
+```shell script
+# from this directory
+npm install
+```
+
+## Run the Application
+
+1. Run docker
+
+    ```shell script
+    # from this directory
+    npm run
+    ```
---
+++ start.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const opentelemetry = require('@opentelemetry/api');
+const { BasicTracerProvider, ConsoleSpanExporter, SimpleSpanProcessor } = require('@opentelemetry/tracing');
+const { CollectorExporter } = require('@opentelemetry/exporter-collector');
+const CollectorProtocolNode = require('@opentelemetry/exporter-collector');
+
+const collectorOptions = {
+  serviceName: 'basic-service',
+  protocolNode: CollectorProtocolNode.HTTP_JSON,
+  url: 'http://0.0.0.0:55680',
+  // url: 'http://localhost:55680/v1/trace' // tried with both urls.
+};
+
+const exporter = new CollectorExporter(collectorOptions);
+
+const provider = new BasicTracerProvider();
+provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+provider.register();
+
+const tracer = opentelemetry.trace.getTracer('example-collector-exporter-node');
+
+// Create a span. A span must be closed.
+const parentSpan = tracer.startSpan('main');
+for (let i = 0; i < 10; i += 1) {
+  doWork(parentSpan);
+}
+// Be sure to end the span.
+parentSpan.end();
+
+// give some time before it is closed
+setTimeout(() => {
+  // flush and close the connection.
+  exporter.shutdown();
+}, 2000);
+
+function doWork(parent) {
+  // Start another span. In this example, the main method already started a
+  // span, so that'll be the parent span, and this will be a child span.
+  const span = tracer.startSpan('doWork', {
+    parent,
+  });
+
+  // simulate some random work.
+  for (let i = 0; i <= Math.floor(Math.random() * 40000000); i += 1) {
+    // empty
+  }
+  // Set attributes to the span.
+  span.setAttribute('key', 'value');
+
+  // Annotate our span to capture metadata about our operation
+  span.addEvent('invoking doWork');
+
+  // end span
+  span.end();
+}
